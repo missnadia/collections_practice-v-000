0999b42cbd9b0e5185c6050ee59400284f271ac0
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-12 23:39:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,6 +6,10 @@ def sort_array_desc(int)
   int.sort {|x, y| y <=> x}
 end
 
+def sort_array_char_count(array)
+  array.sort {|x, y| array[x].length < array[y].length}
+end
+
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
