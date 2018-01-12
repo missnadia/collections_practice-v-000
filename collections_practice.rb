42b8d4294d66205db57fefa751003e9044cd06b5
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-12 23:07:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,9 @@ end
 def sort_array_desc(int)
   int.sort {|x, y| y <=> x}
 end
+
+def swap_elements_from_to(array, index, destination_index)
+  index = array[x]
+  destination_index = array[y]
+  array[x], array[y] = array[y], array[x]
+end
