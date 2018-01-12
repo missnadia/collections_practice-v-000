6aa27f10a74075bbcee6fa90355ad16e42c7c934
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-12 23:48:50 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,13 +15,6 @@ def swap_elements(array)
   array
 end
 
-def swap_elements_from_to(array, index, destination_index)
-  index = array[x]
-  destination_index = array[y]
-  array[x], array[y] = array[y], array[x]
-  array
-end
-
 def reverse_array(array)
   array.reverse
 end
